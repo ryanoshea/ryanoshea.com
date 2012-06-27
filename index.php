5cf72b93b87cb1a13e7253fec3a1abfa3f31f34a
Ryan O'Shea <ryan@ryanoshea.com>
2012-06-26 23:01:59 -0400

    Revert "Revert "added/styled first panel w/ no animation""
    This reverts commit 3b601bc7cf39806dfeb386a69d317ed6f938df1a.

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php include('includes/includes.inc.php'); ?><?php echoPageHead("Home", "home"); ?>
-
+	
+	<div class="panels" id="blog-panel">
+		<iframe src="http://post.ryanoshea.com/"></iframe>
+		<a class="panel-closebutton" href="#">&times;</a>
+	</div>
+	
+	
 	<?php echoPageHeader(); ?>
 	
 			<?php echoBio(); ?>
 					
 				<div id="profile-links"> 
 					<ul>
+						<li>sometimes i <a id="blog-link" href="#" title="my blog" target="_blank">write</a></li>
+						<li>sometimes i <a id="twitter-link" href="#" title="my twitter" target="_blank">babble</a></li>
+						<li>sometimes i <a id="github-link" href="#" title="my github" target="_blank">code</a></li>
+						<li>sometimes i <a id="flickr-link" href="#" title="my flickr" target="_blank">take photos</a></li>
+						<li>sometimes i <a id="facebook-link" href="#" title="my facebook" target="_blank">waste time</a></li>
+						<li>sometimes i <a id="delicious-link" href="#" title="my del.icio.us" target="_blank">save things i like</a></li>
+						<li>sometimes i <a id="google-link" href="#" title="my google+" target="_blank">shout into the void</a></li>
+						<!--
 						<li>sometimes i <a href="//post.ryanoshea.com/" title="my blog" target="_blank">write</a></li>
 						<li>sometimes i <a href="//twitter.com/ryancoshea" title="my twitter" target="_blank">babble</a></li>
 						<li>sometimes i <a href="//github.com/ryanoshea" title="my github" target="_blank">code</a></li>
@@ -13,6 +27,7 @@
 						<li>sometimes i <a href="//facebook.com/ryan.c.oshea" title="my facebook" target="_blank">waste time</a></li>
 						<li>sometimes i <a href="//delicious.com/ryancoshea" title="my del.icio.us" target="_blank">save things i like</a></li>
 						<li>sometimes i <a href="https://plus.google.com/106038738053849300832/posts" title="my google+" target="_blank">shout into the void</a></li>
+						-->
 					</ul>
 					<ul>
 						<li>i went <a href="//devonprep.com" title="my high school" target="_blank">here</a></li>
