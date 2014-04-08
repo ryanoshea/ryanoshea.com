5c79951b386df982611c1c37f1ab8308e7f1d12b
Ryan O'Shea <ryan@ryanoshea.com>
2014-04-08 10:49:26 -0400

    updated GA tracking script

--- includes/includes.inc.php
+++ includes/includes.inc.php
@@ -23,18 +23,14 @@
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
     <!--[if lte IE 7]><script src="/images/elusive-iconfont-master/lte-ie7.js"></script><![endif]-->
 	
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-3319912-4']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-3319912-4', 'example.com');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview');
 	</script>
 </head>
 
