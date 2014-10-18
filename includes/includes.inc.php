67aa6e2336747b72db9543833a0da8d13984b879
Ryan O'Shea <ryan@ryanoshea.com>
2014-10-17 23:55:58 -0400

    Updated for October 2014
    Changed some front page content only.

--- includes/includes.inc.php
+++ includes/includes.inc.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	function echoPageHead($title, $id) {
 ?>
 <!doctype html>
@@ -10,25 +10,25 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>
-        	Ryan O'Shea<?php 
+        	Ryan O'Shea<?php
 			if($title != ""){
 				echo " // $title";
 			} ?>
 	</title>
 	<meta name="description" content="Homepage of Ryan O'Shea: student, programmer, and web developer from Philadelphia, PA">
 	<meta name="viewport" content="width=device-width">
-	<meta name="author" content="Ryan O'Shea"> <!-- href="/humans.txt" --> 
+	<meta name="author" content="Ryan O'Shea"> <!-- href="/humans.txt" -->
 	<link rel="stylesheet" href="/css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,800' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
     <!--[if lte IE 7]><script src="/images/elusive-iconfont-master/lte-ie7.js"></script><![endif]-->
-	
+
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-3319912-4', 'example.com');
+		ga('create', 'UA-3319912-4', 'ryanoshea.com');
 		ga('require', 'displayfeatures');
 		ga('send', 'pageview');
 	</script>
@@ -36,7 +36,7 @@
 
 <body id="<?php if($id != "") {echo $id;} ?>">
 	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-	
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
 	<script src="js/main.js"></script>
@@ -45,12 +45,12 @@
 ?>
 
 
-<?php 
+<?php
 	function echoBio() {
 ?>
 				<h2>
 					<p id="line1">
-						I&#8217;m a student from outside of Philadelphia. 
+						I&#8217;m a student from outside of Philadelphia.
 					</p>
 					<p id="line3">
 						I code, I build websites, I design, and I write.
@@ -62,7 +62,7 @@
 						And that&#8217;s about it.
 					</p>
 				</h2>
-                
+
                 <div id="resume"><a href="/contact/resume.pdf" class="nohover nocolor" target="_blank">r&eacute;sum&eacute; &nbsp;<img src="/images/doc.svg" height=20 width=17></a></div>
 <?php
 	}
@@ -70,17 +70,17 @@
 
 
 
-<?php 
+<?php
 	function echoPageHeader() {
 ?>
-	
+
 	<div id="wrapper">
 		<div id="main" role="main">
         	<div id="left">
                 <h1>
                     <a href="//ryanoshea.com" class="nocolor nohover">Ryan<strong>O&#8217;Shea</strong></a>
                 </h1>
-			</div>                
+			</div>
 			<div id="content">
 <?php
 	}
@@ -88,20 +88,23 @@
 
 
 
-<?php 
+<?php
 	function echoFooter() {
 ?>
-			</div>	
+			</div>
 		</div>
 		<div id="push"></div>
 	</div>
-	
+
 	<footer>
 		<p>
 			<a href="https://github.com/ryanoshea/ryanoshea.com" class="nocolor">see the code for this site on github <i id="icon-github-footer" class="icon-github"></i></a> &nbsp;// Copyright &copy; 2006&ndash;<?php echo date("Y"); ?> Ryan O'Shea. CC <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" class="nocolor" target="_blank">BY-NC-SA</a>.
 		</p>
 		<p>
 			Contact me at <span class="dummy">asdfas</span>ry<span class="dummy">asdfas</span>an@<span class="dummy">7jt30h</span>ryanosh<span class="dummy">asdfas</span>ea.com (<a href="http://pgp.mit.edu/pks/lookup?op=get&search=0x3BE538A4E17D2A03" class="nocolor">PGP key</a>) or via <a href="/contact/bitmessage.txt" class="nocolor">Bitmessage</a>.
+		</p>
+		<p>
+			Bitcoin <span style="text-transform: none;">1KfUtrFCZEDnS7XDYnYd5bJgyP21FuA1yW</span>
 		</p>
 	</footer>
 <?php
@@ -110,7 +113,7 @@
 
 
 
-<?php 
+<?php
 	function echoDocClose() {
 ?>
 </body>
--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     	<a class="panel-closebutton nohover" href="#">&times;</a>
 	</div>
 	<div class="panels panels-small" id="twitter-panel">
-    	
+
         <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/ryancoshea" data-widget-id="345595603637633024">Tweets by @ryancoshea</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
-        
+
         <!-- OLD TWITTER WIDGET CODE
 		<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
 		<script>
@@ -64,56 +64,56 @@
 		<a class="panel-closebutton" href="#">&times;</a>
 	</div>
 	-->
-	
+
 	<?php echoPageHeader(); ?>
-	
+
 			<?php echoBio(); ?>
 
                 <p>
-                    this summer: i&#8217;m <a href="http://www.linkedin.com/pub/ryan-o-shea/36/103/20b/">interning at comcast</a> doing product engineering for business voice
+                    this summer: I <a href="http://www.linkedin.com/pub/ryan-o-shea/36/103/20b/">interned at Comcast</a> building an AngularJS web app &amp; backend for Business Voice
                     <br>
-                    what i&#8217;ve done recently: <a href="http://www.ryanoshea.com/bitcoin">a site</a> about bitcoin for a final project, an <a href="http://ryanoshea.com/scheduler">employee scheduling tool</a> (in progress)
-    			</p>	
-				<div id="profile-links"> 
+                    what else I&#8217;ve done recently: <a href="http://www.ryanoshea.com/bitcoin">a site</a> about bitcoin for a final project
+    			</p>
+				<div id="profile-links">
 					<ul id="profiles">
-						<li>sometimes i <a id="blog-link" href="<?php 
+						<li>sometimes I <a id="blog-link" href="<?php
 							if ($detect->isMobile())
 								echo "http://post.ryanoshea.com";
 							else
 								echo "#";
 						?>" title="my blog">write</a></li>
-                        <li>sometimes i <a id="linkedin-link" href="http://www.linkedin.com/pub/ryan-o-shea/36/103/20b/" title="my linkedin" target="_blank">work</a></li>
-						<li>sometimes i <a id="twitter-link" href="<?php 
+                        <li>sometimes I <a id="linkedin-link" href="http://www.linkedin.com/pub/ryan-o-shea/36/103/20b/" title="my linkedin" target="_blank">work</a></li>
+						<li>sometimes I <a id="twitter-link" href="<?php
 							if ($detect->isMobile())
 								echo "https://twitter.com/ryancoshea";
 							else
 								echo "#";
 						?>" title="my twitter">talk</a></li>
-                        <li>sometimes i <a id="github-link" href="https://github.com/ryanoshea?tab=repositories" title="my github" target="_blank">code</a></li>
-                        <li>sometimes i <a id="behance-link" href="http://www.behance.net/ryanoshea" title="my behance" target="_blank">design</a></li>
-						<li>sometimes i <a id="flickr-link" href="http://flickr.com/rinoshea" title="my flickr" target="_blank">take photos</a></li>
-						<li>sometimes i <a id="facebook-link" href="https://facebook.com/ryan.c.oshea" title="my facebook" target="_blank">waste time</a></li>
-						<li>sometimes i <a id="delicious-link" href="<?php 
+                        <li>sometimes I <a id="github-link" href="https://github.com/ryanoshea?tab=repositories" title="my github" target="_blank">code</a></li>
+                        <li>sometimes I <a id="behance-link" href="http://www.behance.net/ryanoshea" title="my behance" target="_blank">design</a></li>
+						<li>sometimes I <a id="flickr-link" href="http://flickr.com/rinoshea" title="my flickr" target="_blank">take photos</a></li>
+						<li>sometimes I <a id="facebook-link" href="https://facebook.com/ryan.c.oshea" title="my facebook" target="_blank">waste time</a></li>
+						<li>sometimes I <a id="delicious-link" href="<?php
 							if ($detect->isMobile())
 								echo "http://delicious.com/ryancoshea";
 							else
 								echo "#";
 						?>" title="my del.icio.us">save</a></li>
-						<li>sometimes i <a id="google-link" href="https://plus.google.com/106038738053849300832/posts" title="my google+" target="_blank">search</a></li>
+						<li>sometimes I <a id="google-link" href="https://plus.google.com/106038738053849300832/posts" title="my google+" target="_blank">search</a></li>
 						<!--
-						<li>sometimes i <a href="//post.ryanoshea.com/" title="my blog" target="_blank">write</a></li>
-						<li>sometimes i <a href="//twitter.com/ryancoshea" title="my twitter" target="_blank">babble</a></li>
-						<li>sometimes i <a href="//github.com/ryanoshea" title="my github" target="_blank">code</a></li>
-						<li>sometimes i <a href="//flickr.com/rinoshea" title="my flickr" target="_blank">take photos</a></li>
-						<li>sometimes i <a href="//facebook.com/ryan.c.oshea" title="my facebook" target="_blank">waste time</a></li>
-						<li>sometimes i <a href="//delicious.com/ryancoshea" title="my del.icio.us" target="_blank">save things i like</a></li>
-						<li>sometimes i <a href="https://plus.google.com/106038738053849300832/posts" title="my google+" target="_blank">shout into the void</a></li>
+						<li>sometimes I <a href="//post.ryanoshea.com/" title="my blog" target="_blank">write</a></li>
+						<li>sometimes I <a href="//twitter.com/ryancoshea" title="my twitter" target="_blank">babble</a></li>
+						<li>sometimes I <a href="//github.com/ryanoshea" title="my github" target="_blank">code</a></li>
+						<li>sometimes I <a href="//flickr.com/rinoshea" title="my flickr" target="_blank">take photos</a></li>
+						<li>sometimes I <a href="//facebook.com/ryan.c.oshea" title="my facebook" target="_blank">waste time</a></li>
+						<li>sometimes I <a href="//delicious.com/ryancoshea" title="my del.icio.us" target="_blank">save things i like</a></li>
+						<li>sometimes I <a href="https://plus.google.com/106038738053849300832/posts" title="my google+" target="_blank">shout into the void</a></li>
 						-->
 					</ul>
 					<ul>
-                    	<li>i go <a href="//princeton.edu" title="my college" target="_blank">here</a></li>
-						<li>i went <a href="//devonprep.com" title="my high school" target="_blank">here</a></li>
-						<li>i&#8217;m brothers with <a href="//djoshea.com" title="dan o'shea" target="_blank">this guy</a></li>
+                    	<li>I&#8217;m majoring in Electrical Engineering at <a href="//princeton.edu" title="my college" target="_blank">Princeton</a>.</li>
+						<li>I went to <a href="//devonprep.com" title="my high school" target="_blank">Devon Prep</a> before that.</li>
+						<li><a href="//djoshea.com" title="dan o'shea" target="_blank">This guy</a> is my brother.</li>
 					</ul>
 				</div>
 	<?php echoFooter(); ?>
