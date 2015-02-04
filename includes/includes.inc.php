8fe132cf395571ec7528e8b73ad8b5b7f98c53f5
Ryan O'Shea <ryan@ryanoshea.com>
2015-02-04 15:19:48 -0500

    Updated GA tracking script

--- includes/includes.inc.php
+++ includes/includes.inc.php
@@ -29,6 +29,7 @@
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-3319912-4', 'ryanoshea.com');
+		ga('require', 'linkid', 'linkid.js');
 		ga('require', 'displayfeatures');
 		ga('send', 'pageview');
 	</script>
