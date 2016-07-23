cfb1b9827b43538ecddee2748e1859cb86dd8c28
Ryan O'Shea <ryan@ryanoshea.com>
2016-07-23 19:58:15 -0400

    Reorganized files

---
+++ frontend/404.php
@@ -0,0 +1,11 @@
+<?php include('includes/includes.inc.php'); ?><?php echoPageHead("404", "error404"); ?>
+
+	<?php echoPageHeader(); ?>
+			
+        <h2>404</h2>
+        <p>
+            This page, quite unfortunately, doesn&#8217;t exist.
+        </p>
+			
+	<?php echoFooter(); ?>
+<?php echoDocClose(); ?>
