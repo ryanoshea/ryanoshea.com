58168340dde815944101ef6b3c81bc741c52cbe7
Ryan O'Shea <ryan@ryanoshea.com>
2016-07-24 13:22:17 -0400

    Removed HTTP code made useless by HSTS

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,20 @@ var helmet = require('helmet');
 var compression = require('compression');
 var fs = require('fs');
 
+// Setup HTTP2/SPDY/HTTP1.1 Server with TLS/SSL
+
+var app = express();
+// Get certificate
 var key = fs.readFileSync('cert/testserver-key.pem');
 var cert = fs.readFileSync('cert/testserver-cert.pem')
-
 var tlsConfig = {
     key: key,
     cert: cert
 };
+app.use(express.static('../frontend')); // static webserver
+app.use(compression()); // enable gzip
 
-var app = express();
-app.use(express.static('../frontend'));
-app.use(compression());
+// Enable HSTS
 var ONE_YEAR = 31536000000;
 app.use(helmet.hsts({
   maxAge: ONE_YEAR,
@@ -23,10 +26,6 @@ app.use(helmet.hsts({
   force: true
 }));
 
-http.createServer(function (req, res) {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
-}).listen(80);
-
-httpsServer = https.createServer(tlsConfig, app);
+// Start HTTPS Server
+var httpsServer = https.createServer(tlsConfig, app);
 httpsServer.listen(443);
