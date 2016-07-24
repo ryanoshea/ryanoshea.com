06b2596cf735616bd8d50e1beec8afdcc25b5ce8
Ryan O'Shea <ryan@ryanoshea.com>
2016-07-23 22:08:40 -0400

    Removed TLS cipher list as Node 4 adds better ones by default

--- backend/server.js
+++ backend/server.js
@@ -10,27 +10,7 @@ var cert = fs.readFileSync('cert/testserver-cert.pem')
 
 var tlsConfig = {
     key: key,
-    cert: cert,
-    ciphers: 
-      [
-        "ECDHE-RSA-AES256-SHA384",
-        "DHE-RSA-AES256-SHA384",
-        "ECDHE-RSA-AES256-SHA256",
-        "DHE-RSA-AES256-SHA256",
-        "ECDHE-RSA-AES128-SHA256",
-        "DHE-RSA-AES128-SHA256",
-        "HIGH",
-        "!aNULL",
-        "!eNULL",
-        "!EXPORT",
-        "!DES",
-        "!RC4",
-        "!MD5",
-        "!PSK",
-        "!SRP",
-        "!CAMELLIA"
-      ].join(':'),
-    honorCipherOrder: true
+    cert: cert
 };
 
 var app = express();
