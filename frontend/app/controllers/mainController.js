5f8bae8bec5bc9ee1b76cbb28bd194be6fd149cb
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-09 17:43:50 -0400

    Remove copyright from footer to free up space on mobile

--- frontend/app/controllers/mainController.js
+++ frontend/app/controllers/mainController.js
@@ -3,11 +3,7 @@
 
 var cont = angular.module('ryanoshea.controllers');
 
-cont.controller('mainController', function ($scope, $filter, $http, $location) {
-  
-  $scope.currentYear = new Date().getFullYear();
-
-});
+cont.controller('mainController', function ($scope, $filter, $http, $location) {});
 
 })();
 
