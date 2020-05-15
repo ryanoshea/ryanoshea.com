3d4c815d8caa571866c8c8982a0b2b7b664f5e3d
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-15 15:24:20 -0400

    Initial shell with no flickr functionality

---
+++ frontend/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/scss/bootstrap.scss';
+import 'normalize.css/normalize.css';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/regular.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
