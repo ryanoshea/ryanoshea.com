4f3a1d7a7bedb22dd429bc4bf46cf8179be476d7
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-16 14:00:34 -0400

    Component decomposition
    Introuced Biography and moved some styles to Nameplate

---
+++ frontend/src/Components/Biography/Biography.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import './Biography.scss';
+
+const Biography = () => (
+    <div className='biography'>
+        <h2 className='intro-lines'>
+            <p id='line1'>I’m a full-stack software engineer based in Boston.</p>
+            <p id='line2'>I build user-facing products and core infrastructure from the ground up.</p>
+            <p id='line3'>I like writing apps, designing user experiences, and taking photos on the side.</p>
+            <p id='line4'>Say hello if you’d like to chat or work together.</p>
+        </h2>
+
+        <div id='resume'>
+            <a href='/contact/resume.pdf' className='nocolor resume-link' target='_blank' rel='noopener noreferrer'>
+                Résumé &nbsp;<i className='far fa-file-alt'></i>
+            </a>
+        </div>
+
+        <p>
+            Currently, I’m prepping to start a new software engineering position at{' '}
+            <a href='https://chewy.com' target='_blank' rel='noopener noreferrer'>
+                Chewy
+            </a>
+            .
+        </p>
+
+        <p>
+            Until the COVID crisis hit the restaurant industry, I spent a brief while as full-stack web developer at{' '}
+            <a href='https://pos.toasttab.com' target='_blank' rel='noopener noreferrer'>
+                Toast
+            </a>{' '}
+            building React apps backed by Java microservices and DynamoDB.
+        </p>
+
+        <p>
+            Previously, I spent 4 years at{' '}
+            <a href='https://composableanalytics.com' target='_blank' rel='noopener noreferrer'>
+                Composable Analytics
+            </a>{' '}
+            leading a team designing, bootstrapping, and implementing their one-of-a-kind enterprise data orchestration
+            platform on .NET, SQL Server, and AngularJS.
+        </p>
+
+        <p>
+            In 2016, I graduated cum laude from{' '}
+            <a href='https://princeton.edu' target='_blank' rel='noopener noreferrer'>
+                Princeton University
+            </a>{' '}
+            with a <abbr title='Bachelor of Science in Engineering'>B.S.E.</abbr> in Electrical Engineering and a{' '}
+            <abbr title='a minor'>certificate</abbr> in Computer Science. My{' '}
+            <a
+                href="https://github.com/ryanoshea/data-center-monitoring/blob/master/report-roshea/O'Shea_Ryan_Thesis.pdf"
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                senior thesis
+            </a>{' '}
+            was a scalable network of temperature sensors for monitoring data center climates. Interesting projects:{' '}
+            <a href='https://github.com/ryanoshea/car-lab' target='_blank' rel='noopener noreferrer'>
+                Car Lab
+            </a>
+            ,{' '}
+            <a href='https://github.com/ryanoshea/poll-princeton' target='_blank' rel='noopener noreferrer'>
+                PollPrinceton
+            </a>
+            .
+        </p>
+
+        <p>
+            Way back, I interned at{' '}
+            <a href='https://robotics.vecna.com/' target='_blank' rel='noopener noreferrer'>
+                Vecna Technologies
+            </a>{' '}
+            and{' '}
+            <a href='https://business.comcast.com' target='_blank' rel='noopener noreferrer'>
+                Comcast
+            </a>
+            .
+        </p>
+    </div>
+);
+
+export default Biography;
--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -3,6 +3,7 @@ import './Home.scss';
 import classNames from 'classnames';
 import { AppReducer, ACTIONS, AppState, Photo } from '../../State/AppState';
 import PhotosFoldout from '../PhotosFoldout/PhotosFoldout';
+import Biography from '../Biography/Biography';
 import { mobileViewport } from '../../Utils';
 import { FOLDOUT_ANIMATION_PRIMARY_DURATION } from '../../Consts';
 
@@ -40,6 +41,7 @@ const Home = () => {
         }
         return newState;
     };
+
     const [
         {
             photos: {
@@ -54,6 +56,7 @@ const Home = () => {
         dispatch,
     ] = useReducer(reducer, new AppState());
 
+    // Load Flickr portfolio image data
     const flickrDataUrl =
         window.location.hostname === 'ryanoshea.com'
             ? '/api/flickr/most-recent-photos'
@@ -93,6 +96,8 @@ const Home = () => {
         // Open the foldout only if the window is big enough
         if (!mobileViewport()) {
             if (loadingPhotos) {
+                // If user tried to open the foldout before the photo data was loaded, show a loading indicator and hook
+                // onto the in-progress request to open the foldout when it completes.
                 dispatch({
                     type: ACTIONS.ATTEMPT_EARLY_FOLDOUT_OPEN,
                 });
@@ -103,6 +108,7 @@ const Home = () => {
                     scrollFoldoutIntoView();
                 });
             } else {
+                // Data is loaded, just open or close the foldout
                 dispatch({
                     type: photosOpen ? ACTIONS.CLOSE_PHOTOS : ACTIONS.OPEN_PHOTOS,
                 });
@@ -129,79 +135,7 @@ const Home = () => {
 
     return (
         <>
-            <h2 className='bio'>
-                <p id='line1'>I’m a full-stack software engineer based in Boston.</p>
-                <p id='line2'>I build user-facing products and core infrastructure from the ground up.</p>
-                <p id='line3'>I like writing apps, designing user experiences, and taking photos on the side.</p>
-                <p id='line4'>Say hello if you’d like to chat or work together.</p>
-            </h2>
-
-            <div id='resume'>
-                <a href='/contact/resume.pdf' className='nocolor resume-link' target='_blank' rel='noopener noreferrer'>
-                    Résumé &nbsp;<i className='far fa-file-alt'></i>
-                </a>
-            </div>
-
-            <p>
-                Currently, I’m prepping to start a new software engineering position at{' '}
-                <a href='https://chewy.com' target='_blank' rel='noopener noreferrer'>
-                    Chewy
-                </a>
-                .
-            </p>
-
-            <p>
-                Until the COVID crisis hit the restaurant industry, I spent a brief while as full-stack web developer at{' '}
-                <a href='https://pos.toasttab.com' target='_blank' rel='noopener noreferrer'>
-                    Toast
-                </a>{' '}
-                building React apps backed by Java microservices and DynamoDB.
-            </p>
-
-            <p>
-                Previously, I spent 4 years at{' '}
-                <a href='https://composableanalytics.com' target='_blank' rel='noopener noreferrer'>
-                    Composable Analytics
-                </a>{' '}
-                leading a team designing, bootstrapping, and implementing their one-of-a-kind enterprise data
-                orchestration platform on .NET, SQL Server, and AngularJS.
-            </p>
-
-            <p>
-                In 2016, I graduated cum laude from{' '}
-                <a href='https://princeton.edu' target='_blank' rel='noopener noreferrer'>
-                    Princeton University
-                </a>{' '}
-                with a <abbr title='Bachelor of Science in Engineering'>B.S.E.</abbr> in Electrical Engineering and a{' '}
-                <abbr title='a minor'>certificate</abbr> in Computer Science. My{' '}
-                <a
-                    href="https://github.com/ryanoshea/data-center-monitoring/blob/master/report-roshea/O'Shea_Ryan_Thesis.pdf"
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    senior thesis
-                </a>{' '}
-                was a scalable network of temperature sensors for monitoring data center climates. Interesting projects:{' '}
-                <a href='https://github.com/ryanoshea/car-lab' target='_blank' rel='noopener noreferrer'>
-                    Car Lab
-                </a>
-                ,{' '}
-                <a href='https://github.com/ryanoshea/poll-princeton' target='_blank' rel='noopener noreferrer'>
-                    PollPrinceton
-                </a>
-                .
-            </p>
-            <p>
-                Way back, I interned at{' '}
-                <a href='https://robotics.vecna.com/' target='_blank' rel='noopener noreferrer'>
-                    Vecna Technologies
-                </a>{' '}
-                and{' '}
-                <a href='https://business.comcast.com' target='_blank' rel='noopener noreferrer'>
-                    Comcast
-                </a>
-                .
-            </p>
+            <Biography />
             <div id='profile-links'>
                 <ul id='profiles'>
                     <li>
