b274658657586951c29bcc867586d5196345c4f0
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-16 18:01:27 -0400

    Fix GitHub icon

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import 'normalize.css/normalize.css';
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 import '@fortawesome/fontawesome-free/scss/regular.scss';
 import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
 import App from './App';
 
 ReactDOM.render(
