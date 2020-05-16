56833cbde527a3de6fe960527908dff510184200
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-16 15:36:16 -0400

    Add deploy script

---
+++ frontend/scripts/deploy.zsh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+echo
+echo "Creating production front-end build."
+yarn build
+
+echo
+echo "Stopping ryanoshea.com server."
+ssh ryanoshea.com "pkill -9 node"
+
+# Backend
+echo
+echo "Deploying back-end assets."
+rsync -avz -e "ssh" --progress ../backend/server.js ryanoshea.com:/home/ryan/ryanoshea.com/backend --delete
+rsync -avz -e "ssh" --progress ../backend/app.js ryanoshea.com:/home/ryan/ryanoshea.com/backend --delete
+rsync -avz -e "ssh" --progress ../backend/flickrAuth.js ryanoshea.com:/home/ryan/ryanoshea.com/backend --delete
+rsync -avz -e "ssh" --progress ../backend/package.json ryanoshea.com:/home/ryan/ryanoshea.com/backend --delete
+rsync -avz -e "ssh" --progress ../backend/package-lock.json ryanoshea.com:/home/ryan/ryanoshea.com/backend --delete
+
+# Frontend
+echo
+echo "Deploying front-end assets."
+rsync -avz -e "ssh" --progress ../frontend/build/ ryanoshea.com:/home/ryan/ryanoshea.com/frontend/build --delete
+
+echo
+echo "Restoring back-end dependencies and starting ryanoshea.com server."
+ssh ryanoshea.com "./start.sh"
