cd17cef279d43b9bd469374ee1bb9ff0bb0958ee
Ryan O'Shea <ryan@ryanoshea.com>
2020-05-16 14:00:34 -0400

    Component decomposition
    Introuced Biography and moved some styles to Nameplate

--- frontend/src/Components/Nameplate/Nameplate.tsx
+++ frontend/src/Components/Nameplate/Nameplate.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Nameplate.scss';
 
 const Nameplate = () => (
     <div id='left'>
--- frontend/src/Consts.tsx
+++ frontend/src/Consts.tsx
@@ -1,2 +1,6 @@
 export const BOOTSTRAP_BREAKPOINT_XL = 1200;
+/**
+ * The full animation takes 500ms, but we have a spacer that expands to the full size within 100ms to allow the
+ * scrollIntoView to begin while the animation is still playing.
+ */
 export const FOLDOUT_ANIMATION_PRIMARY_DURATION = 100;
