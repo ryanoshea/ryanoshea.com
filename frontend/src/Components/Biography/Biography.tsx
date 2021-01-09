a160c899161be1867be9413f9f31c822c8cb8c08
Ryan O'Shea <ryan@ryanoshea.com>
2021-01-09 14:38:22 -0500

    Add details to Chewy job and static resume timestamp

--- frontend/src/Components/Biography/Biography.tsx
+++ frontend/src/Components/Biography/Biography.tsx
@@ -1,83 +1,88 @@
 import React from 'react';
 import './Biography.scss';
 
-const Biography = () => (
-    <article className='biography'>
-        <h2 className='intro-lines'>
-            <p id='line1'>I’m a full-stack software engineer based in Boston.</p>
-            <p id='line2'>I build user-facing products and core infrastructure from the ground up.</p>
-            <p id='line3'>I like writing apps, designing user experiences, and taking photos on the side.</p>
-            <p id='line4'>Say hello if you’d like to chat or work together.</p>
-        </h2>
+const Biography = () => {
+    const year = new Date().getFullYear();
 
-        <div id='resume'>
-            <a href='/contact/resume.pdf' className='resume-link' target='_blank' rel='noopener noreferrer'>
-                Résumé &nbsp;<i className='far fa-file-alt'></i>
-            </a>
-        </div>
+    return (
+        <article className='biography'>
+            <h2 className='intro-lines'>
+                <p id='line1'>I’m a full-stack software engineer based in Boston.</p>
+                <p id='line2'>I build user-facing products and core infrastructure from the ground up.</p>
+                <p id='line3'>I like writing apps, designing user experiences, and taking photos on the side.</p>
+                <p id='line4'>Say hello if you’d like to chat or work together.</p>
+            </h2>
 
-        <p>
-            I recently started a new senior software engineering position on{' '}
-            <a href='https://chewy.com' target='_blank' rel='noopener noreferrer'>
-                Chewy’s
-            </a>{' '}
-            Pet Health team.
-        </p>
+            <div className='resume'>
+                <a href='/contact/resume.pdf' className='resume-link' target='_blank' rel='noopener noreferrer'>
+                    Résumé &nbsp;<i className='far fa-file-alt'></i>
+                </a>{' '}
+                <em className='resume-timestamp'>Last updated May 2020.</em>
+            </div>
 
-        <p>
-            Until the COVID-19 pandemic hit the restaurant industry, I spent a brief while at{' '}
-            <a href='https://pos.toasttab.com' target='_blank' rel='noopener noreferrer'>
-                Toast
-            </a>{' '}
-            building React apps backed by Java microservices and DynamoDB.
-        </p>
+            <p>
+                {year === 2021 ? 'Last year' : 'In 2021'}, I started as senior software engineer on{' '}
+                <a href='https://www.chewy.com/b/pharmacy-2515' target='_blank' rel='noopener noreferrer'>
+                    Chewy’s Pet Health team
+                </a>
+                , shipping storefront features at scale using Kotlin and Vue.js on Spring Boot.
+            </p>
 
-        <p>
-            Previously, I spent 4 years at{' '}
-            <a href='https://composableanalytics.com' target='_blank' rel='noopener noreferrer'>
-                Composable Analytics
-            </a>{' '}
-            leading a team designing, bootstrapping, and implementing their one-of-a-kind enterprise data orchestration
-            platform on .NET, SQL Server, and AngularJS.
-        </p>
+            <p>
+                Until the COVID-19 pandemic hit the restaurant industry, I spent a brief while at{' '}
+                <a href='https://pos.toasttab.com' target='_blank' rel='noopener noreferrer'>
+                    Toast
+                </a>{' '}
+                building React apps backed by Java microservices and DynamoDB.
+            </p>
 
-        <p>
-            In 2016, I graduated cum laude from{' '}
-            <a href='https://princeton.edu' target='_blank' rel='noopener noreferrer'>
-                Princeton University
-            </a>{' '}
-            with a <abbr title='Bachelor of Science in Engineering'>B.S.E.</abbr> in Electrical Engineering and a{' '}
-            <abbr title='a minor'>certificate</abbr> in Computer Science. My{' '}
-            <a
-                href="https://github.com/ryanoshea/data-center-monitoring/blob/master/report-roshea/O'Shea_Ryan_Thesis.pdf"
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                senior thesis
-            </a>{' '}
-            was a scalable network of temperature sensors for monitoring data center climates. Interesting projects:{' '}
-            <a href='https://github.com/ryanoshea/car-lab' target='_blank' rel='noopener noreferrer'>
-                Car Lab
-            </a>
-            ,{' '}
-            <a href='https://github.com/ryanoshea/poll-princeton' target='_blank' rel='noopener noreferrer'>
-                PollPrinceton
-            </a>
-            .
-        </p>
+            <p>
+                Previously, I spent 4 years at{' '}
+                <a href='https://composableanalytics.com' target='_blank' rel='noopener noreferrer'>
+                    Composable Analytics
+                </a>{' '}
+                leading a team designing, bootstrapping, and implementing their one-of-a-kind enterprise data
+                orchestration platform on .NET, SQL Server, and AngularJS.
+            </p>
 
-        <p>
-            Way back, I interned at{' '}
-            <a href='https://robotics.vecna.com/' target='_blank' rel='noopener noreferrer'>
-                Vecna Technologies
-            </a>{' '}
-            and{' '}
-            <a href='https://business.comcast.com' target='_blank' rel='noopener noreferrer'>
-                Comcast
-            </a>
-            .
-        </p>
-    </article>
-);
+            <p>
+                In 2016, I graduated cum laude from{' '}
+                <a href='https://princeton.edu' target='_blank' rel='noopener noreferrer'>
+                    Princeton University
+                </a>{' '}
+                with a <abbr title='Bachelor of Science in Engineering'>B.S.E.</abbr> in Electrical Engineering and a{' '}
+                <abbr title='a minor'>certificate</abbr> in Computer Science. My{' '}
+                <a
+                    href="https://github.com/ryanoshea/data-center-monitoring/blob/master/report-roshea/O'Shea_Ryan_Thesis.pdf"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    senior thesis
+                </a>{' '}
+                was a scalable network of temperature sensors for monitoring data center climates. Interesting projects:{' '}
+                <a href='https://github.com/ryanoshea/car-lab' target='_blank' rel='noopener noreferrer'>
+                    Car Lab
+                </a>
+                ,{' '}
+                <a href='https://github.com/ryanoshea/poll-princeton' target='_blank' rel='noopener noreferrer'>
+                    PollPrinceton
+                </a>
+                .
+            </p>
+
+            <p>
+                Way back, I interned at{' '}
+                <a href='https://robotics.vecna.com/' target='_blank' rel='noopener noreferrer'>
+                    Vecna Technologies
+                </a>{' '}
+                and{' '}
+                <a href='https://business.comcast.com' target='_blank' rel='noopener noreferrer'>
+                    Comcast
+                </a>
+                .
+            </p>
+        </article>
+    );
+};
 
 export default Biography;
