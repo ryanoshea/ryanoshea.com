12cbe6c22addc04370e120b83b74c8031f07354f
Ryan O'Shea <ryan@ryanoshea.com>
2021-06-12 12:35:16 -0400

    fix(api): Fix backend calls going to localhost in Safari

--- frontend/src/Utils/api.tsx
+++ frontend/src/Utils/api.tsx
@@ -1,3 +1,5 @@
-export const getUrl = (relativePath: string) => window.location.hostname === 'ryanoshea.com'
-  ? relativePath
-  : `https://localhost${relativePath}`;
+// Local dev setup involves backend running on one port and react dev server on another. Ensure backend calls get routed
+// to the local backend instead of the react dev server.
+export const getUrl = (relativePath: string) => window.location.hostname.indexOf('localhost') >= 0
+  ? `https://localhost${relativePath}`
+  : relativePath;
